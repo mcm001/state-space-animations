872d1a3d5281d9ac163d4a4a5d1d58146d0b314b
Matt <matthew.morley.ca@gmail.com>
2020-03-30 16:08:46 -0700

    update install

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
-sudo apt update
-
-sudo apt-get install -qqy --no-install-recommends apt-utils ffmpeg sox libcairo2-dev texlive texlive-fonts-extra texlive-latex-extra texlive-latex-recommended texlive-science tipa
-
 python3.7 -m pip install setuptools
 python3.7 -m pip install -r requirements.txt
 python3.7 -m pip install manimlib
