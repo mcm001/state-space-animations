7919887c8da7ccc7a71436dbc9c1f7c21b79f01c
Matt <matthew.morley.ca@gmail.com>
2020-03-30 12:03:51 -0700

    add explicit python 3.7 pip

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ sudo apt update
 
 sudo apt-get install -qqy --no-install-recommends apt-utils ffmpeg sox libcairo2-dev texlive texlive-fonts-extra texlive-latex-extra texlive-latex-recommended texlive-science tipa
 
-pip3 install setuptools
-pip3 install -r requirements.txt
-pip3 install manimlib
+python3.7 -m pip install setuptools
+python3.7 -m pip install -r requirements.txt
+python3.7 -m pip install manimlib
