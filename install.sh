fdb58cd7f8292904cd88d8b0922f796465e57e78
Matt <matthew.morley.ca@gmail.com>
2020-03-30 11:18:49 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,7 @@
-sudo apt update \
-&& apt-get install -qqy --no-install-recommends \
-apt-utils \
-ffmpeg \
-sox \
-libcairo2-dev \
-texlive \
-texlive-fonts-extra \
-texlive-latex-extra \
-texlive-latex-recommended \
-texlive-science \
-tipa \
-&& pip3 install setuptools
-&& pip3 install -r requirements.txt
-&& pip3 install manimlib
+sudo apt update
+
+sudo apt-get install -qqy --no-install-recommends apt-utils ffmpeg sox libcairo2-dev texlive texlive-fonts-extra texlive-latex-extra texlive-latex-recommended texlive-science tipa
+
+pip3 install setuptools
+pip3 install -r requirements.txt
+pip3 install manimlib
